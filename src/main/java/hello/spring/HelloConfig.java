18fe9315354975f0336d47fc829b6f86e9dacacd
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-07-31 17:19:39 +0900

    스프링 컨테이너 등록

---
+++ src/main/java/hello/spring/HelloConfig.java
@@ -0,0 +1,13 @@
+package hello.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfig {
+    // 직접 컨트롤러 빈 등록
+    @Bean
+    public HelloController helloController() {
+        return new HelloController();
+    }
+}
