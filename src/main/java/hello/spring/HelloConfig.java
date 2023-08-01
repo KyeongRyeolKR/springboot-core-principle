b8982337253567929ea730ea7868ab171da66617
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-01 15:01:56 +0900

    편리한 부트 클래스 만들기

--- src/main/java/hello/spring/HelloConfig.java
+++ src/main/java/hello/spring/HelloConfig.java
@@ -3,7 +3,7 @@ package hello.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class HelloConfig {
     // 직접 컨트롤러 빈 등록
     @Bean
