e10bcb096d5781de3e1c063a9f8bb5b4cd7e95c2
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-02 22:29:09 +0900

    자동 구성 라이브러리 사용

--- src/main/java/hello/boot/config/MemoryConfig.java
+++ src/main/java/hello/boot/config/MemoryConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration // 자동 구성 ON 일 때는 해당 설정 클래스가 필요 없으므로 작동되지 않게 주석 처리
 //@Conditional(MemoryCondition.class)
 //@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
